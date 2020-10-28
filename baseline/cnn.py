18d15078f33a5bfe62cbc5e7fc7fdfac58e7bf89
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-10-28 05:40:49 +0000

    Setup baseline net training

--- baseline/cnn.py
+++ baseline/cnn.py
@@ -2,20 +2,23 @@ import torch.nn as nn
 
 
 class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 4, 7)
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(4, 4, 7)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.relu2 = nn.ReLU()
-        self.fc = nn.Linear(4 * 103 * 67, 10)
-        self.sigmoid = nn.Sigmoid()
+  '''
+  Baseline CNN neural network.
+  '''
+  def __init__(self):
+    super(CNN, self).__init__()
+    self.conv1 = nn.Conv2d(3, 4, 7)
+    self.pool1 = nn.MaxPool2d(2, 2)
+    self.relu1 = nn.ReLU()
+    self.conv2 = nn.Conv2d(4, 4, 7)
+    self.pool2 = nn.MaxPool2d(2, 2)
+    self.relu2 = nn.ReLU()
+    self.fc = nn.Linear(4 * 103 * 67, 10)
+    self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x):
-        x = self.pool1(self.relu1(self.conv1(x)))
-        x = self.pool2(self.relu2(self.conv2(x)))
-        x = x.view(-1, 4 * 103 * 67)
-        x = self.sigmoid(self.fc(x))
-        return x
+  def forward(self, x):
+    x = self.pool1(self.relu1(self.conv1(x)))
+    x = self.pool2(self.relu2(self.conv2(x)))
+    x = x.view(-1, 4 * 103 * 67)
+    x = self.sigmoid(self.fc(x))
+    return x
