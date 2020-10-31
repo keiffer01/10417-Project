7df67027a610f195be118d2f1e1cecd6d2126510
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-10-31 18:30:55 +0000

    Implement script that crops white border from images

---
+++ data_parsing/crop_images.py
@@ -0,0 +1,18 @@
+# Crops the white border from the image dataset
+
+import glob
+from PIL import Image
+
+# Cropped image size that removes the white border, determined by hand
+newSize = (54, 35, 389, 252)
+
+images_path = "../data/images_original"
+genre_paths = glob.glob(images_path + "/*")
+
+for genre_path in genre_paths:
+  image_paths = glob.glob(genre_path + "/*")
+
+  for image_path in image_paths:
+    image = Image.open(image_path)
+    newImage = image.crop(newSize)
+    newImage.save(image_path)
