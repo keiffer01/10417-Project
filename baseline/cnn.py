e27bbccd18e3d02e6f5c5b3c408b179551ff2e1a
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-11-01 06:52:30 +0000

    Time-series CNN

--- baseline/cnn.py
+++ baseline/cnn.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 
-convoluted_image_area = 15 * 217
+convoluted_image_area = 65 * 1
+image_height = 217
 
 class CNN(nn.Module):
   '''
@@ -8,19 +9,15 @@ class CNN(nn.Module):
   '''
   def __init__(self):
     super(CNN, self).__init__()
-    self.conv1 = nn.Conv2d(3, 4, (1, 3))
-    self.pool1 = nn.MaxPool2d((1, 2), (1, 2))
-    self.relu1 = nn.ReLU()
-    self.conv2 = nn.Conv2d(4, 4, (1, 3))
-    self.pool2 = nn.MaxPool2d((1, 2), (1, 2))
-    self.relu2 = nn.ReLU()
-    self.fc = nn.Linear(4 * convoluted_image_area, 10)
+    self.conv = nn.Conv2d(3, 7, (image_height, 3))
+    self.relu = nn.ReLU()
+    self.pool = nn.MaxPool3d((4, 1, 1))
+    self.linear = nn.Linear(convoluted_image_area, 10)
     self.sigmoid = nn.Sigmoid()
 
 
   def forward(self, x):
-    x = self.pool1(self.relu1(self.conv1(x)))
-    x = self.pool2(self.relu2(self.conv2(x)))
-    x = x.view(-1, 4 * convoluted_image_area)
-    x = self.sigmoid(self.fc(x))
+    x = self.pool(self.relu(self.conv(x)))
+    x = x.view(-1, convoluted_image_area)
+    x = self.sigmoid(self.linear(x))
     return x
